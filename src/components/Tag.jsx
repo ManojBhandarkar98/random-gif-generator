f6a1bcad15aa97a0bef14d6d3bd58ebe406dc58b
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-16 19:43:30 +0530

    Epi - 11 - Random GIF - element value attach to onclick by using event.target.value in wirthout function

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -25,17 +25,14 @@ const Tag = () => {
   function clickHandler() {
     fetchData();
   }
-  function changeHandler(event) {
-    const value = event.target.value;
-    setSearch(value);
-  }
+  
   return <div className="w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
     <h1 className="mt=[15px] text-2xl font-bold underline uppercase">Search Random GIF</h1>
     {
       loading ? (<Spinner />) : (<img src={gif} width={450} />)
     }
 
-    <input type='text' className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center' onChange={changeHandler} value={search}>
+    <input type='text' className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center' onChange={(event)=>setSearch(event.target.value)} value={search}>
     </input>
 
     <button onClick={clickHandler} className="w-10/12 bg-white opacity-90 text-lg py-2 rounded-lg mb-[20px]">Generate</button>
