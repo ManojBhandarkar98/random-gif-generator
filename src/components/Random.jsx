bb8bc946458c1ad35ed569682869ba285dd2a42c
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-16 19:41:46 +0530

    Epi - 11 - Random GIF - element value attach to onclick by using event.target.value in function

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -28,7 +28,7 @@ const Random = () => {
     fetchData();
   }
   return <div className="w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
-    <h1 className="mt=[15px] text-2xl font-bold underline uppercase">Random GIF</h1>
+    <h1 className="mt=[15px] text-2xl font-bold underline uppercase">A Random GIF based on Generate button</h1>
     {
       loading ? (<Spinner />) : (<img src={gif} width={450} />)
     }
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
 const Tag = () => {
-  return (
-    <div>Tag</div>
-  )
+  const [gif, setGif] = useState('');
+  const [loading, setLoading] = useState(false); 
+  const [search, setSearch] = useState('');
+
+  async function fetchData() {
+    setLoading(true)
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=2doF8g9OHTSYDcNRojNEyohyDYRcfkWI`;
+    const { data } = await axios.get(url);
+    const imageSource = data.data.images.downsized_large.url;
+    setGif(imageSource);
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData();
+  }
+    , [])
+
+  function clickHandler() {
+    fetchData();
+  }
+  function changeHandler(event) {
+    const value = event.target.value;
+    setSearch(value);
+  }
+  return <div className="w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+    <h1 className="mt=[15px] text-2xl font-bold underline uppercase">Search Random GIF</h1>
+    {
+      loading ? (<Spinner />) : (<img src={gif} width={450} />)
+    }
+
+    <input type='text' className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center' onChange={changeHandler} value={search}>
+    </input>
+
+    <button onClick={clickHandler} className="w-10/12 bg-white opacity-90 text-lg py-2 rounded-lg mb-[20px]">Generate</button>
+  </div>;
 }
 
-export default Tag
+export default Tag;
