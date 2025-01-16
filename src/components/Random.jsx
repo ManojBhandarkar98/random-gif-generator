52296517d5176b5e52a000093d12642324f2139f
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-16 16:32:20 +0530

    Epi - 11 - Random GIF - Random gif components css done

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,3 +1,16 @@
-export default function Random() {
-  return <div>Random</div>;
+import { useState } from "react";
+
+const Random=()=> {
+  const [gif,setGif] = useState('');
+
+  function clickHandler(){
+
+  }
+  return <div className="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+    <h1 className="text-2xl font-bold underline uppercase">Random GIF</h1>
+    <img src={gif} width={450}/>
+    <button onClick={clickHandler} className="w-10/12 bg-white opacity-90 text-lg py-2 rounded-lg">Generate</button>
+  </div>;
 }
+
+export default Random;
